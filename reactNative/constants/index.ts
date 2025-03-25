3ffc9032860f51ce9c33db27accf375a5c8428e8
Yassirho53 <houariyassir32@gmail.com>
2025-03-25 01:21:29 +0000

    Frontend ReactNative

---
+++ reactNative/constants/index.ts
@@ -0,0 +1,41 @@
+import clean1 from "@/assets/images/clean1.avif";
+import clean2 from "@/assets/images/clean2.avif";
+import clean3 from "@/assets/images/clean3.avif";
+
+import elec1 from "@/assets/images/elec1.avif";
+import elec2 from "@/assets/images/elec2.jpg";
+
+
+import onb1 from "@/assets/images/onb1.avif";
+import onb2 from "@/assets/images/onb2.avif";
+import onb2jpg from "@/assets/images/onb2.jpg";
+import onb3 from "@/assets/images/onb3.jpg";
+import onb11 from "@/assets/images/onb11.avif";
+
+import peint1 from "@/assets/images/peint1.avif";
+import peint2 from "@/assets/images/peint2.avif";
+import peint3 from "@/assets/images/peint3.avif";
+
+import plomb1 from "@/assets/images/plomb1.jpg";
+import plomb2 from "@/assets/images/plomb2.avif";
+import plomb3 from "@/assets/images/plomb3.avif";
+
+// Centralisé dans un seul export
+export const images = {
+  clean1,
+  clean2,
+  clean3,
+  elec1,
+  elec2,
+  onb1,
+  onb2,
+  onb2jpg,
+  onb3,
+  onb11,
+  peint1,
+  peint2,
+  peint3,
+  plomb1,
+  plomb2,
+  plomb3,
+};
